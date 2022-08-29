9f7c674827609a1f405ff21456c1f231881aaf38
nonpa <nonpanpila@gmail.com>
2022-08-29 19:15:29 +0700

    update @types for ExStore.

--- src/lib/@types/ExStore.d.ts
+++ src/lib/@types/ExStore.d.ts
@@ -1,66 +1,7 @@
 import type { Writable } from 'svelte/store';
 
 /**
- * @param {string} type - the type of the action, will be displayed in the Redux DevTools.
- * @param {any} payload - the payload data.
- */
-export type Action<T = any> = { type: string; payload?: T };
-
-/**
- * @param {string} state - current state.
- * @param {Action} action - action to be applied.
- */
-export type Reducer<T = any> = (state?: T, action?: Action) => void;
-
-/**
- * @param name - name of the store, will be displayed in Redux DevTools.
- * @param initialValue - initial value state of the store.
- * @param reducers - reducers to be applied to the store.
- */
-export interface ExSlice<T = any> {
-	name: string;
-	initialValue: T;
-	reducers: {
-		[key: string]: Reducer<T>;
-	};
-}
-
-/**
  * ExStore is a Svelte Store that can be used to create Redux-like stores.
- *
- * @example
- * <caption>Create a slice</caption>
- * ```typescript
- *	const count = exSlice({
- *		name: 'count',
- *		initialValue: 0,
- *		reducers: {
- *			increase: (state) {
- *				state += 1;
- *			},
- *			decrease: (state) {
- *				state -= 1;
- *			},
- *			increaseByValue: (state, action) {
- *				state += action.payload;
- *			}
- *		}
- *	})
- * ```
+ * with the integration of ExStoreEnhancer for example, the Redux DevTools extension.
  */
-export type CreateExSlice<T = any> = (
-	slice: ExSlice<T>,
-	enhancers?: ExStoreEnhancer<T>[] = []
-) => ExSlice<T>;
-
-/**
- * ExStore is a Svelte Store that can be used to create Redux-like stores.
- * with the integration of the Redux DevTools extension.
- */
-export type ExStore = Writable<T> & ExSlice<T>;
-
-/**
- * @param {ExStore} store - the store to be enhanced.
- * @returns {ExStore} enhanced store.
- */
-export type ExStoreEnhancer<T = any> = (store: ExSlice<T>) => (store: ExSlice) => ExSlice<T>;
+export type ExStore<T> = Writable<T> & ExSlice<T>;
