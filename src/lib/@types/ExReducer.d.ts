aeade61f1ce34ae3e04d0ed1f613cde0115de58e
nonpa <nonpanpila@gmail.com>
2022-08-31 06:29:41 +0700

    style: remove unused code.

--- src/lib/@types/ExReducer.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Action } from './Action';
-
-/**
- * @param {string} state - current state.
- * @param {Action} action - action to be applied.
- */
-export type Reducer<T = any> = (state?: T, action?: Action) => void;
