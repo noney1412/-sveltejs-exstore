7e7bb3a8f6c4e7c60d779fdf0daf1ccd3035f5ea
nonpa <nonpanpila@gmail.com>
2022-09-03 06:31:42 +0700

    add exSlice object type.

--- src/lib/types/ExSlice.d.ts
+++ src/lib/types/ExSlice.d.ts
@@ -1,58 +1,14 @@
-import type { Reducer } from './ExReducer';
-import type { ExStore } from './Store';
-import type { ExStoreEnhancer } from './ExStoreEnhancer';
+import type { OnlyPrimitive } from './utils';
 
 /**
- *	@param name - name of the store, will be displayed in Redux DevTools.
- *	@param initialValue - initial value state of the store.
- *	@param reducers - reducers to be applied to the store.
- *
- *	@example
- *	```typescript
- *	const countSlice: ExSlice<number> = {
- *			name: 'count',
- *			initialValue: 0,
- *			reducers: {
- *				increment: (state: number, action: Action) => { state + 1 },
- *				decrement: (state: number, action: Action) => { state - 1 }
- *			  increaseBy: (state: number, action: Action<number>) => { state + action.payload }
- *			}
- *	}
- *	```
+ * exSlice is the parameter object for exStore.
+ * @param name is the name of the store, which is used in devtools.
+ * @initialValue is the initial value of the store by type State.
+ * @actions is the function that contains all the actions of the store.
  */
-export interface ExSlice<TState = any> {
+export type ExSlice<State> = {
 	name: string;
-	initialValue: TState;
-	reducers: {
-		[key: string]: Reducer<TState>;
-	};
-}
+	initialValue: OnlyPrimitive<State>;
+	actions: (update: Writable<OnlyPrimitive<State>>['update']) => OnlyFunc<State>;
+};
 
-/**
- * ExStore is a Svelte Store that can be used to create Redux-like stores.
- * @param {ExSlice} slice - the slice of the store.
- * @param {ExStoreEnhancer} [enhancers] - enhancers to be applied to the store.
- * @example
- * <caption>Create a slice</caption>
- * ```typescript
- * const count = exSlice({
- * 	name: 'count',
- * 	initialValue: 0,
- * 	reducers: {
- * 		increase: (state) {
- * 			state += 1;
- * 		},
- * 		decrease: (state) {
- * 			state -= 1;
- * 		},
- * 		increaseByValue: (state, action) {
- * 			state += action.payload;
- * 		}
- * 	}
- * })
- * ```
- */
-export type CreateExSlice<T = any> = (
-	slice: ExSlice<T>,
-	enhancers?: ExStoreEnhancer<T>[] = []
-) => ExStore<T>;
