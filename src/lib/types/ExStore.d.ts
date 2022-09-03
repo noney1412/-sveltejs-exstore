9c1a589369f8466d9cd83bd3079056f3b65977a8
nonpa <nonpanpila@gmail.com>
2022-09-03 07:52:07 +0700

    replace type ReturnExStore<State> with ExStore<State> instead.

--- src/lib/types/ExStore.d.ts
+++ src/lib/types/ExStore.d.ts
@@ -2,19 +2,15 @@ import type { Writable } from 'svelte/store';
 import type { ExSlice } from './ExSlice';
 import type { OnlyFunc, OnlyPrimitive } from './utils';
 
-
-type ExStore<State> =  Writable<OnlyPrimitive<State>> & OnlyFunc<State>;
-
-/** 
- * ReturnType of the {@link CreateExStore<State>}
- * @alias {@link ExStore<State>}
+/**
+ * ExStore is redux-like store.
  * @typeParam State - initial value.
-*/
-type ReturnExStore<State> = Writable<OnlyPrimitive<State>> & OnlyFunc<State>;
+ */
+export type ExStore<State> =  Writable<OnlyPrimitive<State>> & OnlyFunc<State>;
 
 /**
  * Create a single store.
  * @typeParam State - type of the initial state.
  * @param {ExSlice<State>} slice is the parameter object to init exStore.
  */
-export type CreateExStore<State> = (slice: ExSlice<State>) => ReturnExStore<State>;
+export type CreateExStore<State> = (slice: ExSlice<State>) => ExStore<State>;
