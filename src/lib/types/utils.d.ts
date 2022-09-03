2444285290cee37e590c9d50f7814580c9d68b7e
nonpa <nonpanpila@gmail.com>
2022-09-03 07:12:59 +0700

    style: capital letter.

--- src/lib/types/utils.d.ts
+++ src/lib/types/utils.d.ts
@@ -1,16 +1,16 @@
 /**
- * extract function type from type object.
+ * Extract function type from type object.
  */
 export type ExtractFunctionFromObject<Props> = {
 	[Key in keyof Props]: Props[Key] extends AnyVoidFunction ? Key : never;
 }[keyof Props];
 
 /**
- * pick only function type from type object.
+ * Pick only function type from type object.
  */
 export type OnlyFunc<T> = Pick<T, ExtractFunctionFromObject<T>>;
 
 /**
- * pick only primitive type from type object.
+ * Pick only primitive type from type object.
  */
 export type OnlyPrimitive<T> = Omit<T, ExtractFunctionFromObject<T>>;
