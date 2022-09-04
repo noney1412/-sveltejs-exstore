d8f15b1c3c5e4a40de1083ff3085107f583f9645
nonpa <nonpanpila@gmail.com>
2022-09-05 06:25:29 +0700

    chore: remove unused import.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,6 @@
 import { writable } from 'svelte/store';
 import type { ExSlice } from './types/ExSlice';
 import type { CreateExStore } from './types/ExStore';
-import type { OnlyPrimitive } from './types/utils';
 
 const exStore: CreateExStore = <State>(slice: ExSlice<State>) => {
 	const { subscribe, update, set } = writable<typeof slice['initialValue']>(slice.initialValue);
