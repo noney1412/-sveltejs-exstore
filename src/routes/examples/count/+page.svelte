d7fe040f8cc4463b0772b18d6855ca7b494836b9
nonpa <nonpanpila@gmail.com>
2022-09-04 15:33:16 +0700

    add <Count /> as an example component.

---
+++ src/routes/examples/count/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Count from '$examples/basic/count/Count.svelte';
+</script>
+
+<div class="page" w="screen" h="screen">
+	<div class="page-content" w="full" h="full" flex="~" justify="center" align="items-center">
+		<Count />
+	</div>
+</div>
+
+<style lang="scss">
+	:root {
+		@apply "<sm:(text-12px) md:(text-16px) desktop:(text-18px)";
+	}
+</style>
