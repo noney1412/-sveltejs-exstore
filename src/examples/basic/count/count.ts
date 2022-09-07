369a2ef41feee9c2090c68be074bc0262d651d97
nonpa <nonpanpila@gmail.com>
2022-09-07 17:03:14 +0700

    update count.ts

--- src/examples/basic/count/count.ts
+++ src/examples/basic/count/count.ts
@@ -10,12 +10,10 @@ export interface Count {
 export const count = exStore<Count>({
 	name: 'count',
 	initialValue: 0,
-	actions: (state, update) => ({
-		increase: () => {
-			update((state) => state + 1);
-		},
-		increaseBy: (by) => state + by,
-		decrease: () => state - 1,
+	actions: (_, update) => ({
+		increase: () => update((state) => state + 1),
+		increaseBy: (by) => update((state) => state + by),
+		decrease: () => update((state) => state - 1),
 		reset() {
 			0;
 		}
