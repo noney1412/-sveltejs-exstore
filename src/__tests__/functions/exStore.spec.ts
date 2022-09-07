5ea4c58c8384557ad52cf5e10d06af6d56f1004c
nonpa <nonpanpila@gmail.com>
2022-09-07 16:15:52 +0700

    test(functions/exStore): add more test cases for primitive type and reference type.

--- src/__tests__/functions/exStore.spec.ts
+++ src/__tests__/functions/exStore.spec.ts
@@ -1,5 +1,5 @@
 import exStore from '$lib';
-import { get } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 
 test('stage 1: with primitive initialValue', () => {
 	interface Count {
@@ -10,10 +10,8 @@ test('stage 1: with primitive initialValue', () => {
 	const count = exStore<Count>({
 		name: 'count-test-store',
 		initialValue: 0,
-		actions: (state) => ({
-			increase() {
-				return state + 1;
-			}
+		actions: (state, update) => ({
+			increase: () => update((state) => state + 1)
 		})
 	});
 
@@ -119,4 +117,80 @@ test('stage 5: without actions', () => {
 	});
 
 	expect(get(count)).toBe(0);
+});
+
+test('stage 6: subscription of the primitive type', () => {
+	// add primitive type to the store
+	interface Count {
+		$initialValue: number;
+		increase: () => void;
+		increaseBy: (by: number) => void;
+		decrease: () => void;
+	}
+
+	const count = exStore<Count>({
+		name: 'count-test-store',
+		initialValue: 0,
+		actions: (_, update) => ({
+			increase: () => update((state) => state + 1),
+			increaseBy: (by) => update((state) => state + by),
+			decrease: () => update((state) => state - 1)
+		})
+	});
+
+	// and subscribe to it
+	const unsubscribe = count.subscribe((value) => {
+		console.log('what is the value from subscription?', value);
+	});
+
+	// precondition
+	expect(get(count)).toBe(0);
+	// double update to make sure the subscription is working
+	count.increase();
+	expect(get(count)).toBe(1);
+	count.increaseBy(2);
+	expect(get(count)).toBe(3);
+	count.decrease();
+	expect(get(count)).toBe(2);
+
+	// postcondition
+	unsubscribe();
+	count.increase();
+});
+
+test('stage 7: subscription of the reference type', () => {
+	interface Profile {
+		name: string;
+		age: number;
+		changeName: (name: string) => void;
+	}
+
+	const profile = exStore<Profile>({
+		name: 'profile-test-store',
+		initialValue: {} as Profile,
+		actions: (state) => ({
+			changeName(name: string) {
+				state.name = name;
+			}
+		})
+	});
+
+	const unsubscribe = profile.subscribe((value) => {
+		console.log('what is the value from subscription?', value);
+	});
+
+	// precondition
+	expect(get(profile)).toEqual({});
+	profile.changeName('John');
+	expect(get(profile)).toEqual({ name: 'John' });
+
+	profile.update((state) => {
+		state.age = 20;
+		return state;
+	});
+
+	expect(get(profile)).toEqual({ name: 'John', age: 20 });
+	// postcondition
+	unsubscribe();
+	profile.changeName('Jane');
 });
