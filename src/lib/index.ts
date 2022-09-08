a2023d49610dc190ca5035f5631427f882101ff7
nonpa <nonpanpila@gmail.com>
2022-09-08 18:47:37 +0700

    refactor: wrap a statement into local function, defineState and createAction.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -9,13 +9,7 @@ function exStore<State>(slice: ExSlice<State>) {
 
 	let state: ExState<State> = {} as ExState<State>;
 
-	if (initialValue instanceof Object) {
-		state = initialValue as ExState<State>;
-	} else {
-		state = {
-			current: initialValue
-		} as ExState<State>;
-	}
+	defineState();
 
 	type WrappedActions = OnlyFunc<State> & {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -24,24 +18,52 @@ function exStore<State>(slice: ExSlice<State>) {
 
 	const actions = slice.actions?.(state) as WrappedActions;
 
-	if (actions && actions instanceof Object) {
-		for (const key in actions) {
-			const fn = actions[key] as (...args: unknown[]) => void | InitialValue<State>;
-			actions[key as keyof WrappedActions] = function (...args: unknown[]) {
-				store.update((current) => {
-					if (current instanceof Object) {
-						fn(...args);
-						return current;
-					} else {
-						state.current = fn(...args) as InitialValue<State>;
-						return state.current ?? current;
-					}
-				});
-			};
+	wrapAction();
+
+	return { subscribe: store.subscribe, update: store.update, set: store.set, ...actions };
+
+	/**
+	 * Define state if the initial value is primitive or reference type
+	 * - Primitive type: number, string, boolean, null, undefined
+	 * - Reference type: object, array, function
+	 * if the initial value is a primitive type, the state will be `{ current: initialValue }`
+	 * if the initial value is a reference type, the state will be `initialValue`
+	 */
+	function defineState() {
+		if (initialValue instanceof Object) {
+			state = initialValue as ExState<State>;
+		} else {
+			state = {
+				current: initialValue
+			} as ExState<State>;
 		}
 	}
 
-	return { subscribe: store.subscribe, update: store.update, set: store.set, ...actions };
+	/**
+	 * Wrap all action functions in order to custom update behavior.
+	 * (only primitive type) If the action function returns a value, the store will be updated with the returned value.
+	 * (only primitive type) state.current will be updated with the returned value.
+	 * (reference type) no value returned. state will be updated within the actions.
+	 */
+	function wrapAction() {
+		if (actions && actions instanceof Object) {
+			for (const key in actions) {
+				const fn = actions[key] as (...args: unknown[]) => void | InitialValue<State>;
+				actions[key as keyof WrappedActions] = function (...args: unknown[]) {
+					// actions is called here
+					store.update((current) => {
+						if (current instanceof Object) {
+							fn(...args);
+							return current;
+						} else {
+							state.current = fn(...args) as InitialValue<State>;
+							return state.current ?? current;
+						}
+					});
+				};
+			}
+		}
+	}
 }
 
 export default exStore;
