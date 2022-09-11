aea17c70623e2a6a0486093a730f84004b2a6ef3
nonpa <nonpanpila@gmail.com>
2022-09-11 19:05:29 +0700

    Fixes AB#118 make the devtool can access the same instance id.

--- src/lib/middlewares/withReduxDevtool.ts
+++ src/lib/middlewares/withReduxDevtool.ts
@@ -1,4 +1,3 @@
-import { browser, dev } from '$app/environment';
 import type { Middleware } from '$lib/types/ExMiddleware';
 import { get } from 'svelte/store';
 import { isReadyForBrowser } from './utils';
@@ -13,6 +12,7 @@ interface WithReduxDevtoolsOption {
 	 * @default 100
 	 */
 	latency?: number;
+	instanceId?: number;
 }
 
 function initDevtool(options: WithReduxDevtoolsOption = { name: 'anonymous', latency: 100 }) {
@@ -25,6 +25,12 @@ function initDevtool(options: WithReduxDevtoolsOption = { name: 'anonymous', lat
 	return devTools;
 }
 
+function getTitle() {
+	if (!isReadyForBrowser()) return;
+
+	return window.document.title;
+}
+
 function getDevtool() {
 	if (!isReadyForBrowser()) return;
 
@@ -34,6 +40,11 @@ function getDevtool() {
 const middlewareByName = new Map();
 
 function withReduxDevtool<State>(middleware: Middleware<State>) {
+	const root = {
+		devTool: initDevtool({ name: getTitle() ?? 'no title', instanceId: 1 }),
+		init: false
+	};
+
 	update();
 	initStore();
 
@@ -43,32 +54,21 @@ function withReduxDevtool<State>(middleware: Middleware<State>) {
 		if (middlewareByName.has(middleware.storeName)) return;
 		middlewareByName.set(middleware.storeName, middleware);
 
-		const devTools = initDevtool({
-			name: 'document.title'
-		});
+		if (!root.devTool) return;
 
-		if (!devTools) return;
-
-		devTools.init(middleware.initialState);
-
-		devTools.subscribe((message: any) => {
-			console.log(message);
-		});
+		if (root.init === false) {
+			root.devTool.init({});
+			root.init = true;
+		}
 	}
 
 	function update() {
 		if (middlewareByName.has(middleware.storeName)) {
-			const devTools = getDevtool();
+			const devTools = initDevtool({ name: 'count', instanceId: 1 });
 
 			if (!devTools) return;
 
-			console.log(middleware.storeName);
-
-			devTools.send({ type: middleware.currentActionName }, get(middleware.store), [
-				{
-					name: middleware.storeName
-				}
-			]);
+			devTools.send({ type: middleware.currentActionName }, get(middleware.store));
 		}
 	}
 }
