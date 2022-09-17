73ad5771cbb1bf9f00fb35ab09b72466ff95fbe5
nonpa <nonpanpila@gmail.com>
2022-09-17 21:20:08 +0700

    Add SharedState type for getState().

--- src/__tests__/concepts/v2/factories/initState.ts
+++ src/__tests__/concepts/v2/factories/initState.ts
@@ -1,18 +1,26 @@
 import type { ExSlice, Extensions } from '../types/ExSlice';
+import type { OnlyState } from '../types/Utils';
+
+interface SharedState<T> {
+	name: string;
+	bind: Partial<OnlyState<T>>;
+	mode: 'primitive' | 'reference';
+}
 
 export function initState<State>(slice: ExSlice<State>) {
-	const state = {
+	const state: SharedState<State> = {
 		name: 'anonymous',
 		bind: {},
 		mode: 'primitive'
 	};
 
 	state.bind = getState(slice);
+	state.mode = 'primitive';
 
 	return state;
 }
 
-export function getState<State>(slice: ExSlice<State>) {
+export function getState<State>(slice: ExSlice<State>): SharedState<State>['bind'] {
 	const options: Array<keyof Extensions> = ['$name', '$options'];
 
 	const isNotFunctionAndNotOptions = (key: string, value: unknown) =>
@@ -22,7 +30,14 @@ export function getState<State>(slice: ExSlice<State>) {
 		isNotFunctionAndNotOptions(key, value)
 	);
 
-	const state = Object.fromEntries(filtered);
+	const state = Object.fromEntries(filtered) as SharedState<State>['bind'];
 
 	return state;
+}
+
+export function analyzeMode<State>(slice: ExSlice<State>): SharedState<State>['mode'] {
+	// if $init.$init is instance of object, then it is a reference.
+	// else then it is primitive.
+
+	return slice;
 }
