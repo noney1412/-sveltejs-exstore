ac4e29cd74198b2f8d444d6c7ba68de98987d10d
nonpa <nonpanpila@gmail.com>
2022-09-19 09:17:28 +0700

    Add actions and return it with store.

--- src/__tests__/concepts/v2/ex.ts
+++ src/__tests__/concepts/v2/ex.ts
@@ -1,12 +1,16 @@
-import { writable } from 'svelte/store';
-import { initSharedState } from './factories/initSharedState';
+import { Writable, writable } from 'svelte/store';
+import { bindActions, initSharedState } from './factories/initSharedState';
 import type { ExSlice } from './types/ExSlice';
+import type { OnlyFunc } from './types/Utils';
 
 export function ex<State>(slice: ExSlice<State>) {
 	// extract state from slice.
 	const state = initSharedState(slice);
 
+	const actions = bindActions(state.bind, slice);
+
 	const store = writable(state.initialState);
 
-	return store;
+	return { ...store, ...actions, ...state } as OnlyFunc<State> &
+		Writable<typeof state.initialState>;
 }
