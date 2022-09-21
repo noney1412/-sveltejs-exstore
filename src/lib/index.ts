6d43eaf141449685c364938ddfe5d3800ad89184
nonpa <nonpanpila@gmail.com>
2022-09-21 13:46:18 +0700

    Rename `initSharedState` to `storeCreator`.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 import { get, writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
-import { initSharedState, getActions } from './initSharedState';
+import { initSharedState, getActions } from './storeCreators';
 import withReduxDevtool from './middlewares/withReduxDevtools';
 import type { ExMiddleware } from './types/ExMiddleware';
 import type { ExSlice } from './types/ExSlice';
