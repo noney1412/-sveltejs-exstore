ef0ac43a0bf191fcc40643e3de88a35fb0174ebe
nonpa <nonpanpila@gmail.com>
2022-09-22 11:32:56 +0700

    Add NPM Component to test.

---
+++ src/examples/basic/npm/NPM.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { count } from './npm';
+</script>
+
+<h1>{$count}</h1>
+<!--  $count is an alias for $count.$init  -->
+
+<button on:click={count.increase}>+</button>
+<button on:click={() => count.increaseBy(5)}>+</button>
+<button on:click={count.reset}>reset</button>
