4c0bf0a3d2255193476507c436d3dac18e10cb29
nonpa <nonpanpila@gmail.com>
2022-09-22 13:25:50 +0700

    Remove $app/env

--- src/lib/middlewares/utils.ts
+++ src/lib/middlewares/utils.ts
@@ -1,7 +1,4 @@
-import { browser, dev } from '$app/environment';
-
 export const isReadyForBrowser = () => {
-	if (!browser && !dev) return false;
 	if (!(typeof window !== 'undefined' && window)) return false;
 
 	return true;
