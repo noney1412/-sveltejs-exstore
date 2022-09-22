638f29e298ac468614933fa1eb18759f9b895355
nonpa <nonpanpila@gmail.com>
2022-09-22 13:50:47 +0700

    Dump $app/env

--- src/lib/middlewares/utils.ts
+++ src/lib/middlewares/utils.ts
@@ -1,5 +1,9 @@
 export const isReadyForBrowser = async () => {
-	const env = await import('$app/environment');
+	const env = {
+		browser: !import.meta.env.SSR,
+		dev: __SVELTEKIT_DEV__
+	};
+
 	if (env) if (!env.browser && !env.dev) return false;
 	if (!(typeof window !== 'undefined' && window)) return false;
 
