f1246790383c54c0afaafa8c3b25c92e710a562d
nonpa <nonpanpila@gmail.com>
2022-09-22 13:54:40 +0700

    Update

--- src/lib/middlewares/utils.ts
+++ src/lib/middlewares/utils.ts
@@ -1,10 +1,10 @@
 export const isReadyForBrowser = async () => {
 	const env = {
-		browser: !import.meta.env.SSR,
-		dev: __SVELTEKIT_DEV__
+		browser: !import.meta.env.SSR || true,
+		dev: __SVELTEKIT_DEV__ || true
 	};
 
-	if (env) if (!env.browser && !env.dev) return false;
+	if (!env.browser && !env.dev) return false;
 	if (!(typeof window !== 'undefined' && window)) return false;
 
 	return true;
