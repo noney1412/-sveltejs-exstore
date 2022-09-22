27762ab302040b219fbcb3051b4ddf4cfa670b08
nonpa <nonpanpila@gmail.com>
2022-09-22 12:36:19 +0700

    Update tsconfig.

--- src/examples/basic/npm/NPM.svelte
+++ src/examples/basic/npm/NPM.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
-	import { count } from './npm';
+	import { ex } from 'svelte-exstore';
+
+	interface Count {
+		$init: number;
+		increase(): void;
+		decrease(): void;
+		increaseBy(by: number): void;
+		reset(): void;
+	}
+
+	const count = ex<Count>({
+		$name: 'count', // `$name` will be displayed in the devtools as a store name.
+		$init: 0,
+		increase: function () {
+			this.$init += 1; // retrieve your current state with `this` keyword.
+		},
+		increaseBy: function (by) {
+			this.$init += by;
+		},
+		decrease: function () {
+			this.$init -= 1;
+		},
+		reset: function () {
+			this.$init = 0;
+		}
+	});
 </script>
 
 <h1>{$count}</h1>
