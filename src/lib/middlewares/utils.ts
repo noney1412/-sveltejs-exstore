518046db0c4bd15b5d28bb807bbe08d3f64e7c1c
nonpa <nonpanpila@gmail.com>
2022-09-22 13:57:32 +0700

    Remove $app/environment.

--- src/lib/middlewares/utils.ts
+++ src/lib/middlewares/utils.ts
@@ -1,10 +1,4 @@
 export const isReadyForBrowser = async () => {
-	const env = {
-		browser: !import.meta.env.SSR ?? true,
-		dev: __SVELTEKIT_DEV__ ?? true
-	};
-
-	if (!env.browser && !env.dev) return false;
 	if (!(typeof window !== 'undefined' && window)) return false;
 
 	return true;
